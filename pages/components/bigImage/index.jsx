ea823b23733a004a515a2e32336122a40988165b
Jovarasleo <jovarasleonavicius@gmail.com>
2022-04-14 17:38:48 +0300

    redone how image is loaded to bigImage components, adding images to localstorage now persists

--- pages/components/bigImage/index.jsx
+++ pages/components/bigImage/index.jsx
@@ -2,20 +2,8 @@ import styles from "./index.module.css";
 import { useEffect, useState, useContext } from "react";
 import FavouritesContext from "../../../context/FavouritesContext";
 let istrue;
-function BigImage({ image, onClick, portraitImages, landscapeImages }) {
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
+function BigImage({ image, onClick }) {
   const { favourites, toggleFavourite } = useContext(FavouritesContext);
-
-  // useEffect(() => {
-  //   let allImages = portraitImages.concat(landscapeImages);
-  //   allImages.filter((file) => {
-  //     if (file?.id === image) {
-  //       setData(file);
-  //       istrue = file;
-  //     }
-  //   });
-  // }, [data]);
   const isFavourite = favourites.includes(image?.id);
   return (
     <div className={styles.imageBackdrop} onClick={onClick}>
